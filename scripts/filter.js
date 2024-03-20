ac26e75d927e943b255fa954d4a2e2dd285e632e
Dan Labrador <thonydaniellelabrador@gmail.com>
2024-03-20 15:45:53 +0800

    fix: create logic to identify if filter inputs match the criteria

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,20 +1,34 @@
 import renderRow from './renderRow.js';
 
-const addFilterFunction = (delegates, tableBody) => {
-  const filterBtn = document.getElementById('filter-btn');
-  filterBtn.addEventListener('click', (event) => {
-    const { name, gender, position, ageMin, ageMax } = document.forms[1];
-    console.log(name.value)
-    event.preventDefault();
+const filterTable = (delegates, input, tableBody) => {
+  const { name, gender, position, ageMin, ageMax } = document.forms[1];
+  input.addEventListener('change', ()=>{
     tableBody.innerHTML = '';
     for (let delegate of delegates) {
-      if (delegate.fullName.toUpperCase().includes(name.value.toUpperCase())) {
+      let includesName = !delegate.fullName || delegate.fullName.toUpperCase().includes(name.value.toUpperCase());
+      let matchesGender = !delegate.gender || delegate.gender.toUpperCase() === gender.value.toUpperCase();
+      let matchesPosition = !delegate.position || delegate.position.toUpperCase() === position.value.toUpperCase();
+      let withinAgeRange =  (!ageMin.value && !ageMax.value) ||
+                            (ageMin.value <= delegate.age && !ageMax.value) ||
+                            (!ageMin.value && delegate.age <= ageMax.value) ||
+                            (ageMin.value <= delegate.age <= ageMax.value);
+
+      console.log(withinAgeRange)
+      if (includesName && matchesGender && matchesPosition && withinAgeRange) {
         const row = document.createElement("tr");
         renderRow(delegate, row);
         tableBody.appendChild(row);
       }
     }
   })
+}
+
+const addFilterFunction = (delegates, tableBody) => {
+  const { name, gender, position, ageMin, ageMax } = document.forms[1];
+  for (let input of document.forms[1]){
+    filterTable(delegates, input, tableBody)
+  }
+
 }
 
 export default addFilterFunction;
